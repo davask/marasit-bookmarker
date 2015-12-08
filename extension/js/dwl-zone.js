46ef9a05f4fd1b6bb3f768dd984e8e0b47ccaf74
davask <davask.42@gmail.com>
2015-12-08 17:57:36 +0100

    v1.6

---
+++ extension/js/dwl-zone.js
@@ -0,0 +1,11 @@
+// var appScope = angular.element('[ng-app="dwlApp"] [ng-view]').scope();
+
+// var appZone = zone.fork(appScope.initBookmark);
+
+// appZone.fork({
+//   '+appScope.initBookmark': function () {
+//     console.log('cya l8r');
+//   }
+// }).run(function () {
+//   // do stuff
+// });
