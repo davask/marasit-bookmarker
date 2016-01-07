38e2db36acfd06164c9bab96d3b201d2eee72c06
davask <davask.42@gmail.com>
2016-01-07 16:06:33 +0100

    delete page extension

--- page/js/devtools.js
+++
@@ -1,6 +0,0 @@
-chrome.devtools.panels.create("davask web limited - chrome bookmarker",
-    "img/icon16.png",
-    "templates/views/devtools_tab.html",
-    function() {
-    }
-);
--- page/js/dwl-app-service.js
+++
@@ -1,7 +0,0 @@
-dwlApp.service('tagService', [function () {
-
-    var _this = this;
-
-    return _this;
-
-}]);
