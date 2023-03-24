8d0d51558336762bb29e2bb7c295c47723b7c237
davask <contact@davask.com>
2023-03-24 17:20:14 +0100

    update webpack to have live extension dev env

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,65 @@
+const webpack = require('webpack');
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
+const config = {
     mode: 'development', // or 'production'
     entry: {
+        hotreload: 'react-hot-loader/patch',
         background: './src/background.js',
         popup: './src/popup.js',
         content: './src/content.js'
     },
     output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname + '/dist')
+        filename: 'assets/js/[name].js',
+        path: path.resolve(__dirname + '/build')
     },
-    resolve: {
-        extensions: ['*', '.js', '.jsx'],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.svg$/,
+                use: 'file-loader'
+            },
+            {
+                test: /\.png$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            mimetype: 'image/png'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    devServer: {
+        devMiddleware: {
+            writeToDisk: true
+        },
+        static: {
+            directory: './build'
+        }
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new CopyPlugin({
-            patterns: [
-                { from: './build', to: './' }
-            ]
+            patterns: [{ from: './public/', to: './' }],
         })
     ]
 };
+
+module.exports = config;
